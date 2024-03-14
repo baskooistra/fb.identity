a1bb4071037f5b52de5c40aff453788b005fa34c
Bas Kooistra <bas.kooistra@iodigital.com>
2024-03-14 11:09:11 +0100

    Fixed coinfiguration issue

--- Identity.API/Program.cs
+++ Identity.API/Program.cs
@@ -7,13 +7,34 @@ using Identity.Domain.Models;
 using Identity.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Identity.API.SeedData;
+using Azure.Core.Pipeline;
+using Azure.Data.AppConfiguration;
+
+const string AppConfigurationKey = "AppConfiguration";
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        var endpoint = builder.Configuration.GetValue<string>(AppConfigurationKey);
+        var configurationKey = $"{builder.Environment.ApplicationName}:";
+        var environmentName = builder.Environment.EnvironmentName;
+
+        Guard.IsNotNullOrWhiteSpace(endpoint);
+        var config = options.Connect(new Uri(endpoint), new ManagedIdentityCredential())
+            .Select($"{configurationKey}*", environmentName)
+            .TrimKeyPrefix(configurationKey);
+    });
+
+    builder.Services.AddApplicationInsightsTelemetry();
+}
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("IdentityContextConnection") ??
                        throw new InvalidOperationException("Connection string 'IdentityContextConnection' not found.");
-
+connectionString = builder.Configuration.GetValue<string>("Identity:ConnectionStrings:IdentityContextConnection");
 builder.Services.AddDbContext<IdentityContext>(dbContextOptions =>
     dbContextOptions.UseSqlServer(connectionString, 
         sqlServerOptions =>
@@ -32,17 +53,6 @@ builder.Services.AddRazorPages();
 
 if (builder.Environment.IsDevelopment())    
     await SeedData.Initialize(builder.Services.BuildServiceProvider());
-else
-{
-    builder.Configuration.AddAzureAppConfiguration(options =>
-    {
-        var endpoint = builder.Configuration.GetValue<string>("ConfigurationEndpoint");
-        Guard.IsNotNullOrWhiteSpace(endpoint);
-        options.Connect(new Uri(endpoint), new ManagedIdentityCredential());
-    });
-    
-    builder.Services.AddApplicationInsightsTelemetry();   
-}
 
 var app = builder.Build();
 
